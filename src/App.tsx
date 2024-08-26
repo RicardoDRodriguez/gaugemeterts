976ef228e8ecdbd54e63a3ac225dcef4aa78da6b
RicardoDRodriguez <ricardo.domingos@gmail.com>
2024-08-26 19:40:24 -0300

    inicio do projeo gaugemetter em TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Container>
+      <style>
+        {`
+          body {
+            max-width: 600px; /* Defina o tamanho máximo desejado */
+            margin: 0 auto; /* Centraliza a página no centro */
+          }
+        `}
+      </style>
+      <Row className="justify-content-md-center">
+        <Col md={12} className="text-center">
+        <h3>Participômetro</h3>
+          <h5>(Distribuição dos Tempos de Fala)</h5>
+          <br/><br/><br/>
+        </Col>
+      </Row>
+
+      {/* Linha para LiveGaugeChart */}
+      <Row className="justify-content-md-center">
+        <Col md={6} className="text-center">
+          
+        </Col>
+      </Row>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+
+
+      {/* Linha para AvatarProgress */}
+      <Row className="justify-content-md-center">
+        <Col md={6} className="text-center">
+         
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
